4846cdde547bb72930a97fc0f9e196df135599cd
Usman Suleiman <usmansbk@gmail.com>
2020-12-17 13:04:36 +0100

    refactor: Decompose search

--- src/App.js
+++ src/App.js
@@ -18,7 +18,21 @@ class BooksApp extends React.Component {
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? <Search /> : <Shelves />}
+        {this.state.showSearchPage ? (
+          <Search />
+        ) : (
+          <div className="list-books">
+            <div className="list-books-title">
+              <h1>MyReads</h1>
+            </div>
+            <Shelves />
+            <div className="open-search">
+              <button onClick={() => this.setState({ showSearchPage: true })}>
+                Add a book
+              </button>
+            </div>
+          </div>
+        )}
       </div>
     );
   }
