ccc27497b2165b47bd2aa87e3e7d9b491b1fe926
Usman Suleiman <usmansbk@gmail.com>
2020-12-17 13:03:28 +0100

    refactor: Decompose screens

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 // import * as BooksAPI from './BooksAPI'
 import "./App.css";
 import Shelves from "./Shelves";
+import Search from "./Search";
 
 class BooksApp extends React.Component {
   state = {
@@ -17,44 +18,7 @@ class BooksApp extends React.Component {
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <button
-                className="close-search"
-                onClick={() => this.setState({ showSearchPage: false })}
-              >
-                Close
-              </button>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author" />
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid" />
-            </div>
-          </div>
-        ) : (
-          <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <Shelves />
-            <div className="open-search">
-              <button onClick={() => this.setState({ showSearchPage: true })}>
-                Add a book
-              </button>
-            </div>
-          </div>
-        )}
+        {this.state.showSearchPage ? <Search /> : <Shelves />}
       </div>
     );
   }
